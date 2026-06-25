6dea92d44924514cff667b3c3f1c3eadc52418d9
Ghina RF <ghinarf9@gmail.com>
2026-06-25 09:25:09 +0700

    fix: bersihkan config vite dan tambah cleanUrls vercel

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,5 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-        matches: resolve(__dirname, 'matches.html'),
-        teams: resolve(__dirname, 'teams.html'),
-        schedule: resolve(__dirname, 'schedule.html'),
-      },
-    },
-  },
+  // Polosan aja tanpa build rollupOptions dulu biar Vercel ga sensitif
 })
